c79c884641bd81d36a971fa88c1cd719a33d3471
Иванов Дмитрий <monsgt-r@yandex.ru>
2019-12-26 18:05:44 +0700

    frontend Navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, {useContext} from "react";
+import {NavLink, useHistory} from "react-router-dom";
+import {AuthContext} from "../context/AuthContext";
+
+export const Navbar = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+
+    const  logoutHandler = (e) => {
+        e.preventDefault()
+        auth.logout()
+        history.push()
+    };
+
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Logo</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/create">Создать</NavLink></li>
+                    <li><NavLink to="/links">Создать</NavLink></li>
+                    <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
